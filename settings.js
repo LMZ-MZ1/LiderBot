b126bd5f892774de7f56e1a0dbf03411cb52545b
Metal-Zero/One <55085359+LMZ-MZ1@users.noreply.github.com>
2026-01-02 21:30:04 -0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,55 @@
+/*
+ # ------------√ ×------------
+    # Agradecimientos :: ZyxlJs
+    # Agradecimientos :: Destroy
+    # Agradecimientos :: AzamiJs
+    # Agradecimientos :: GataDios
+	# Agradecimientos :: LMZ
+
+    - Recuerda dejar los creditos, no quites los creditos de los autores del código!
+    - Puedes modificar esta base a tu gusto, recuerda dejar los creditos correspondiente!
+ # ------------√ ×------------
+*/
+
+// # Esta base contiene la edición de Sub-Bots, este código fué hecho por ZyxlJs!
+
+import fs from 'fs';
+import chalk from 'chalk';
+
+global.owner = ['5492916450307']
+
+global.sessionName = 'Sessions/Owner'
+global.version = '^3.0'
+global.pairing_code = true
+global.number_bot = ''
+global.prefa = '/'
+global.msgglobal = '[Error: *TypeError*] fetch failed'
+globalThis.dev = '☄︎ ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴛᴇʟʟᴀʀ'
+
+global.api = {
+  url: 'https://api.stellarwa.xyz',
+  key: 'Diamond'
+}
+
+global.bot = {
+  api: 'https://api.stellarwa.xyz',
+  web: 'https://bot.stellarwa.xyz'
+}
+
+global.mods = [
+  '5492916450307'
+]
+
+global.mess = {
+  socket: '🌱 Este comando solo puede ser ejecutado por un Socket.',
+  admin: '🌱 Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+  botAdmin: '🌱 Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+}
+// ================================
+// Clash Royale API (Clan War)
+// ================================
+global.clashRoyale = {
+  baseUrl: 'https://api.clashroyale.com/v1',
+  currentWarEndpoint: '/clans/%23QGVQPY0Y/currentwar',
+  apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFjOTBiZDA0LTZmYWMtNDY2OC1hN2M4LTE3NzZkMjk5MGY2ZSIsImlhdCI6MTc2NzQwODc3MCwic3ViIjoiZGV2ZWxvcGVyL2RhNWJhYWFiLWVkYjQtMTRjNS1iN2Q1LTA1YmUxMjA1YTI5NiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODkuMTk0LjEwLjE4MSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.YUkISaNmloSynC0lmraX0Smfc9Ru3mSs7mh0MN0isTJ0qdeeOV2F7t798E507Bbbmx15yRdvCUbW5zCeTNzTwQ'
+}

3b2cdbeaea0088f4f2d8fbb6a38e2e1ac3433b0c
Metal-Zero/One <55085359+LMZ-MZ1@users.noreply.github.com>
2026-01-03 14:15:52 -0600

    Add files via upload

--- handler.js
+++ handler.js
@@ -21,7 +21,7 @@ import initDB from './lib/system/initDB.js';
 import antilink from './commands/antilink.js';
 import level from './commands/level.js';
 import { getGroupAdmins } from './lib/message.js';
-import warReminder from './commands/clash/recordatorio.js';'; // ajusta la ruta
+import warReminder from './commands/clash/recordatorio.js';// ajusta la ruta
 
 client.on('ready', () => {
   warReminder(client);  // <-- activa los recordatorios
